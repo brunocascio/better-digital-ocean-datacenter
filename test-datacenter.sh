c733cc1aaa5e85051761714a236795dbbb9cc1bf
Bruno Cascio <brunocascio@gmail.com>
2016-06-28 19:36:36 -0300

    Uploaded script

---
+++ test-datacenter.sh
@@ -0,0 +1,28 @@
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+better_ping=99999
+better_datacenter=''
+better_datacenter_name=''
+max_ping=$1
+
+while read -r line;
+do
+	echo -n "Ping ${bold}${line}${normal}: "
+	ping_avg=`ping -c $1 $line | tail -n1 | cut -d'=' -f2 | cut -d'/' -f2`
+	echo "$ping_avg ms"
+	if [[ $ping_avg < $better_ping ]];
+	then
+		better_ping=$ping_avg
+		better_datacenter=$line
+		better_datacenter_name=`echo $line | cut -d'.' -f1 | cut -d'-' -f2`
+	fi
+done < "$2"
+
+echo ""
+echo "${bold}---------------------- Better Datacenter is -----------------------------------${normal}"
+echo "${bold}Domain${normal}: $better_datacenter"
+echo "${bold}Ping AVG${normal}: $better_ping ms"
+echo "${bold}Name${normal}: $better_datacenter_name"
+echo "${bold}-------------------------------------------------------------------------------${normal}"
+echo ""
