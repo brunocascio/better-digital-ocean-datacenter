c7134d75a22d2698136d08f79835b43d0ba6b7c0
Bruno Cascio <brunocascio@gmail.com>
2016-07-10 17:45:11 -0300

    Replaces <<< to echo '' | cmd

--- test-datacenter.sh
+++ test-datacenter.sh
@@ -13,7 +13,7 @@ do
 	echo -n "Ping ${bold}${line}${normal}: "
 	ping_avg=`ping -c $1 $line | tail -n1 | cut -d'=' -f2 | cut -d'/' -f2`
 	echo "$ping_avg ms"
-	if [ $(bc <<< "$ping_avg < $better_ping") -eq 1 ];
+	if [ $(echo "$ping_avg < $better_ping" | bc) -eq 1 ];
 	then
 		better_ping=$ping_avg
 		better_datacenter=$line
